171c15e67fc51a74524a157b7d61a64be3bcd8ab
Marcus Salopaso <114456732+marcusxro@users.noreply.github.com>
2025-01-02 18:50:39 +0800

    Update mongodb.ts

--- src/app/lib/mongodb.ts
+++ src/app/lib/mongodb.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://marcussalopaso1:zedmain1525@cluster0.m8fd2iw.mongodb.net/Mingle')
+        await mongoose.connect('
         console.log("Connected to MongoDB")
 
     }catch (err) {
